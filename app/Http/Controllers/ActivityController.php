cf59724455ab853dcd806b2207b6222c8521348a
Unknown <892368206@qq.com>
2018-07-18 00:41:34 +0800

    update
    更新title

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use App\Activity;
 use App\UserActivity;
+use App\User;
 
 class ActivityController extends Controller{
 
@@ -25,11 +26,15 @@ class ActivityController extends Controller{
      */
 	public function projectCreate(Request $request){
 
+		$activity = new Activity;
+
 		//活动的主题
 		$title = $request->input('title');
 
 		if(is_null($title))
 			$title = "";
+
+		$activity->title = $title;
 
 		//活动时间（设置成为，某时，某刻某分）
 		$time = $request->input('time');
@@ -37,11 +42,15 @@ class ActivityController extends Controller{
 		if(is_null($time))
 			$time = "";
 
+		$activity->time = $time;
+
 		//活动的具体位置
 		$location = $request->input('location');
 
 		if(is_null($location))
 			$location = "";
+
+		$activity->location = $location;
 
 		//活动的经纬度（现在该字段用来表明活动的省市县）
 		$latitude_longitude = $request->input('latitude_longitude');
@@ -49,12 +58,32 @@ class ActivityController extends Controller{
 		if(is_null($latitude_longitude))
 			$latitude_longitude = "";
 
-		$token = $request->input('token');//token不可能为null,token为空的话就没办法进入这个controller
+		$activity->latitude_longitude = $latitude_longitude;
 
-		$picture_url = $request->input('picture_url');
+		//token不可能为null,token为空的话就没办法进入这个controller,根据token获取到user
+		$token = $request->input('token');
+		$user = User::where('remember_token',$token)->get();
+		$activity->init_user_id = $user->id;
+
+
+		//获取照片的url
+		$picture_url = $request->input('picture');
 
 		if(is_null($picture_url))
 			$picture_url = "";//如果picture_url为空的话，就使用默认图片
+		$activity->picture_url = $picture_url;
+
+		//获取活动描述
+		$description = $request->input('description');
+
+		if(is_null($description))
+			$description = "该用户很懒，什么都没填。";
+		$activity->description = $description;
+
+		var_dump($activity->save());
+
+		//返回调用response
+		return $activityController ->response_cjj($activity,'200','success');
 
 	}
 
@@ -65,6 +94,66 @@ class ActivityController extends Controller{
      * @return Response
      */
 	public function projectUpdate(Request $request){
+
+		//获取id
+		$id = $request->input('id');
+
+		//根据id获取activity
+		$activity = Activity::find($id);
+
+		//活动的主题
+		$title = $request->input('title');
+
+		$activity->title = $title;
+
+		//活动时间（设置成为，某时，某刻某分）
+		$time = $request->input('time');
+
+		if(is_null($time))
+			$time = "";
+
+		$activity->time = $time;
+
+		//活动的具体位置
+		$location = $request->input('location');
+
+		if(is_null($location))
+			$location = "";
+
+		$activity->location = $location;
+
+		//活动的经纬度（现在该字段用来表明活动的省市县）
+		$latitude_longitude = $request->input('latitude_longitude');
+
+		if(is_null($latitude_longitude))
+			$latitude_longitude = "";
+
+		$activity->latitude_longitude = $latitude_longitude;
+
+		//token不可能为null,token为空的话就没办法进入这个controller,根据token获取到user
+		$token = $request->input('token');
+		$user = User::where('remember_token',$token)->get();
+		$activity->init_user_id = $user->id;
+
+
+		//获取照片的url
+		$picture_url = $request->input('picture');
+
+		if(is_null($picture_url))
+			$picture_url = "";//如果picture_url为空的话，就使用默认图片
+		$activity->picture_url = $picture_url;
+
+		//获取活动描述
+		$description = $request->input('description');
+
+		if(is_null($description))
+			$description = "该用户很懒，什么都没填。";
+		$activity->description = $description;
+
+		var_dump($activity->save());
+
+		//返回调用response
+		return $activityController ->response_cjj($activity,'200','success');
 		
 	}
 
