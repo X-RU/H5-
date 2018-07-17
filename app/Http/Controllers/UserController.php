b90f168db1e1b4f113965bfcc70498b87eb453e1
Unknown <892368206@qq.com>
2018-07-17 10:23:33 +0800

    Project Begin
    ok, our project begin

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use App\Student;
+
+class UserController extends Controller{
+
+	public function test3(){
+
+		// all()方法查询所有数据
+		$studnets=Student::all();
+		// dd($studnets);
+
+		//find()查询一条，依据主键查询。findOrFail()查找不存在的记录时会抛出异常
+		$student=Student::find(5);  //主键为5的记录
+		var_dump($student);
+
+		//查询构造器的使用,省略了指定表名
+		$student=Student::get();  
+		var_dump($student);
+
+	}
+
+}
