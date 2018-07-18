297b6ff9cc4f5dc5abfb15c215c1fbecceadfb7c
MaXiaoQuan <31005736+MaXiaoQuan@users.noreply.github.com>
2018-07-18 09:41:13 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        <meta property="wb:webmaster" content="187ec29670282439" />
+        
+        <title>咱们趣玩吧</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
