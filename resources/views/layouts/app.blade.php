2c69ea5be20eaa84c910baf70fcd03da2eeda0e8
MaXiaoQuan <31005736+MaXiaoQuan@users.noreply.github.com>
2018-07-18 09:30:16 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>趣玩吧</title>
+    <title>咱们趣玩吧</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
