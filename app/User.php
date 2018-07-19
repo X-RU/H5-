0f83db5f7ecc84b68ff6b4b3ad762f019f469622
Unknown <892368206@qq.com>
2018-07-19 20:05:53 +0800

    更新

--- app/User.php
+++ app/User.php
@@ -2,11 +2,14 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    /**
+    use Notifiable;
+
+    /*
      * The database table used by the model.
      *
      * @var string
@@ -20,14 +23,14 @@ class User extends Model
      */
     protected $primaryKey = 'id';
 
-    //设置主键是非递增的，是可以自己设置的。
+    // 设置主键是非递增的，是可以自己设置的。
     public $incrementing = false;
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['id','idStr','screen_name','province','city','location','description','profile_Image_url','profile_url','gender','remark','avatar_hd','online_status','lang','create_at','update_at'];
+    protected $fillable = ['id','idStr','screen_name','province','city','location','description','profile_Image_url','profile_url','gender','remark','avatar_hd','online_status','lang'];
 
     /**
      * The attributes that should be hidden for arrays.
