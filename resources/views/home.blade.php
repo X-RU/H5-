b2007bfc446c3b4e74cc3b83ef0a44305fd6ae14
MaXiaoQuan <1826034784@qq.com>
2018-07-25 09:24:29 +0800

    modify a syntax -> 'name is undefined'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,4 +23,4 @@
       //     //TODO redirect
       //     window.location = '/user/weibo_login'
       //   }
-      // })</script><link href=/static/css/app.b7ca27f652042c547b0bf7def65f0d49.css rel=stylesheet></head><body><div id=header></div><div id=app></div><div id=footer></div><script type=text/javascript src=/static/js/manifest.2ae2e69a05c33dfc65f8.js></script><script type=text/javascript src=/static/js/vendor.93147effff07df3e9d8e.js></script><script type=text/javascript src=/static/js/app.b609c0b799e938bdeb35.js></script></body></html>
+      // })</script><link href=/static/css/app.8f4d6faee6d61bd84069e003b5e180d2.css rel=stylesheet></head><body><div id=header></div><div id=app></div><div id=footer></div><script type=text/javascript src=/static/js/manifest.2ae2e69a05c33dfc65f8.js></script><script type=text/javascript src=/static/js/vendor.93147effff07df3e9d8e.js></script><script type=text/javascript src=/static/js/app.d8b3da30bf4c584ba1fa.js></script></body></html>
